2b03564f73678a2db403a22b1a56224218afebcf
Armando Montero <111711434+m00nbug@users.noreply.github.com>
2023-03-18 14:12:45 -0400

    Add files via upload

---
+++ Grabs_Security_Logs_from_7_days.ps1
@@ -0,0 +1,14 @@
+﻿$today = Get-Date
+$weekAgo = $today.AddDays(-7)
+$username = $env:USERNAME
+$logName = "Security"
+
+$events = Get-WinEvent -FilterHashtable @{LogName=$logName;StartTime=$weekAgo;EndTime=$today;ID=4624,4634,4625,4728,4732,4756,1102,4740,4663;Level=0} -ErrorAction SilentlyContinue |
+Where-Object {$_.Properties[5].Value -eq $username}
+
+if ($events) {
+    $events | Format-Table TimeCreated, Id, Message -AutoSize
+}
+else {
+    Write-Host "No security events found for user $username in the last week." -ForegroundColor Yellow
+}
---
+++ Script_Execution_Disable_For_NonAdminUsers.ps1
@@ -0,0 +1,11 @@
+﻿# Check if the current user is an administrator
+
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") # If the current user is not an administrator, disable PowerShell script execution
+
+if (!$isAdmin) {
+    Write-Host "Disabling PowerShell script execution by non-admin users..."
+    Set-ExecutionPolicy Restricted -Scope CurrentUser
+    Write-Host "PowerShell script execution disabled for non-admin users."
+} else {
+    Write-Host "Current user is an administrator. No action taken."
+}
