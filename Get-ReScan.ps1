71ef38a04a0922f1d4d03c63879bc114c52d25c3
Armando Montero <111711434+m00nbug@users.noreply.github.com>
2023-03-18 15:20:57 -0400

    Update Get-ReScan.ps1

--- Get-ReScan.ps1
+++ Get-ReScan.ps1
@@ -6,7 +6,7 @@
     Copyright = '2020 - 2023 SimeonOnSecurity. All rights reserved.'
     Description = 'Get VirusTotal report for a specified file. Ex. Get-VirusScan -api {string} -file {string}'
     GUID = '12664c93-57f4-4cd0-b8cd-350bfd69891f'
-    PowerShellVersion = '6.0'
+    PowerShellVersion = '7.0'
     ModuleVersion = '1.0.1'
         # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
         PrivateData = @{
