3d8f21b7d495ea667f2ce0e6fc98109ee66272a7
Armando Montero <111711434+m00nbug@users.noreply.github.com>
2023-03-18 14:17:04 -0400

    Update Grabs_Security_Logs_from_7_days.ps1

--- Grabs_Security_Logs_from_7_days.ps1
+++ Grabs_Security_Logs_from_7_days.ps1
@@ -1,13 +1,14 @@
-﻿$today = Get-Date
+$today = Get-Date
 $weekAgo = $today.AddDays(-7)
 $username = $env:USERNAME
 $logName = "Security"
 
-$events = Get-WinEvent -FilterHashtable @{LogName=$logName;StartTime=$weekAgo;EndTime=$today;ID=4624,4634,4625,4728,4732,4756,1102,4740,4663;Level=0} -ErrorAction SilentlyContinue |
+$events = Get-WinEvent -FilterHashtable @{LogName=$logName;StartTime=$weekAgo;EndTime=$today;ID=4624,4634;Level=0} -ErrorAction SilentlyContinue |
 Where-Object {$_.Properties[5].Value -eq $username}
 
 if ($events) {
-    $events | Format-Table TimeCreated, Id, Message -AutoSize
+    $events | Format-Table TimeCreated, Id, Message -AutoSize | Out-File -FilePath "C:\log.txt"
+    Write-Host "Security events saved to C:\log.txt"
 }
 else {
     Write-Host "No security events found for user $username in the last week." -ForegroundColor Yellow
